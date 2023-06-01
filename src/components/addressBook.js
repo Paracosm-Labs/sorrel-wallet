8d018a933eb518d3a8bdd5797bf5739ec6329571
CosmicAnon <wescosmic@gmail.com>
2023-06-01 19:26:49 +0545

    Revert "updates and qr reader test"
    This reverts commit 4ac6454ae46519897acbed239736d863647c56be.

--- src/components/addressBook.js
+++ src/components/addressBook.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { QrReader } from 'react-qr-reader';
+import React from 'react';
 
 const AddressBook = () => {
   const dummyContacts = [
@@ -9,99 +8,32 @@ const AddressBook = () => {
     { name: 'Marcus Toussaint', avatar: 'https://i.pravatar.cc/42?img=4' },
   ];
 
-  const [selectedOption, setSelectedOption] = useState(null);
-  const [scannedQR, setScannedQR] = useState(null);
-
-  const handleOptionClick = (option) => {
-    setSelectedOption(option);
-    setScannedQR(null); // Reset the scanned QR when selecting a new option
-  };
-
-  const handleScanQR = (data) => {
-    if (data) {
-      setScannedQR(data);
-    }
-  };
-
-  const handleError = (error) => {
-    console.error(error);
-  };
-
   return (
     <div>
+
       <div className="mt-3 d-flex address-book">
-        <div className="align-items-center m-2">
-          <button className="btn btn-sm h-100 btn-outline-secondary" onClick={() => handleOptionClick('qr')}>
+          <div className="align-items-center m-2">
+          <button className="btn btn-sm h-100 kbtn-outline-secondary">
             <i className="fa-solid fa-qrcode"></i>
             <small>Scan QR</small>
           </button>
-        </div>
-        <div className="align-items-center m-2">
-          <button className="btn btn-sm h-100 btn-outline-secondary" onClick={() => handleOptionClick('nfc')}>
-            <i className="fa-brands fa-nfc-symbol"></i>
-            <small>Wallet NFC Card</small>
+          </div>
+          <div className="align-items-center m-2">
+          <button className="btn btn-sm h-100 kbtn-outline-secondary">
+            <i className="fa-solid fa-address-book"></i>
+            <small>My Contacts</small>
           </button>
-        </div>
+          </div>
         {dummyContacts.map((contact, index) => (
-          <div key={index} className="align-items-center m-2">
-            <button
-              className="btn btn-sm h-100 btn-outline-secondary"
-              onClick={() => handleOptionClick(contact)}
-            >
-              <img src={contact.avatar} alt={contact.name} className="rounded-circle" width="50" height="50" />
-              <small className="text-muted">{contact.name}</small>
-            </button>
+          <div key={index} className="align-items-cente m-2">
+          <button className="btn btn-sm h-100 kbtn-outline-secondary">
+            <img src={contact.avatar} alt={contact.name} className="rounded-circle" width="50" height="50" />
+            <small className="text-muted">{contact.name}</small>
+          </button>
           </div>
         ))}
+
       </div>
-
-      {selectedOption === 'qr' && (
-        <div className="mt-3">
-          <div className="text-center">
-            <h5>To:</h5>
-            {scannedQR ? (
-              <>
-                <p>Scanned QR: {scannedQR}</p>
-                {/* Additional logic to process the scanned QR and update the "To" section */}
-              </>
-            ) : (
-              <QrReader
-                delay={300}
-                onError={handleError}
-                onScan={handleScanQR}
-                style={{ width: '300px' }}
-              />
-            )}
-          </div>
-        </div>
-      )}
-
-      {selectedOption === 'nfc' && (
-        <div className="mt-3">
-          <div className="text-center">
-            <h5>To:</h5>
-            <p>Wallet NFC Card</p>
-            {/* Additional logic for handling the Wallet NFC Card */}
-          </div>
-        </div>
-      )}
-
-      {dummyContacts.includes(selectedOption) && (
-        <div className="mt-3">
-          <div className="text-center">
-            <h5>To:</h5>
-            <img
-              src={selectedOption.avatar}
-              alt={selectedOption.name}
-              className="rounded-circle"
-              width="100"
-              height="100"
-            />
-            <p>{selectedOption.name}</p>
-            {/* Additional logic for handling the selected contact */}
-          </div>
-        </div>
-      )}
     </div>
   );
 };
--- src/components/searchBox.js
+++ src/components/searchBox.js
@@ -8,7 +8,7 @@ const SearchBox = () => {
     <div className="input-group px-4 mt-3">
       <input type="text" className="form-control form-control-lg" placeholder="Search..." />
     </div>
-      <button className="btn btn-outline-secondary w-50 mt-3 m-auto">Discover</button>
+      <button className="btn btn-lg btn-outline-secondary">Discover</button>
     </div>
     </div>
 
