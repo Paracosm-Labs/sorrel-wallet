331fb49f3e556db3e9cdfb07b633cb66d03986db
CosmicAnon <wescosmic@gmail.com>
2023-06-06 02:55:15 +0545

    Update explorePage.js

--- src/pages/explorePage.js
+++ src/pages/explorePage.js
@@ -14,7 +14,7 @@ const ExplorePage = () => {
     { id: 1, username: 'Food & Entertainment', image: 'https://media.discordapp.net/attachments/1061348902252597368/1113937679789338644/wescosmic_table_of_consumer_products_for_instagram_0_d75d5b92-5939-44f0-9139-850581ecbaf8.png?width=350&height=180' },
     { id: 2, username: 'Products & Services', image: 'https://media.discordapp.net/attachments/1061348902252597368/1113937981301076078/wescosmic_consumer_electronics_in_packaging_for_instagram_2_87e17f12-6521-480c-9039-d7d418133cc8.png?width=350&height=180' },
     { id: 3, username: 'Travel & Experiences', image: 'https://media.discordapp.net/attachments/1061348902252597368/1113933972314009610/wescosmic_caribbean_travel_experiences_for_instagram_0_131b0149-9c3d-420f-b140-b9dca00e80f3.png?width=350&height=180' },
-    { id: 4, username: 'Education', image: 'https://media.discordapp.net/attachments/1061348902252597368/1115385822343069706/wescosmic_smart_online_classroom_for_university_students_future_e568a396-ea0d-4a3f-8b65-7ec3cfe82931.png?width=350&height=180' },
+    { id: 4, username: 'Courses & XR-Learning', image: 'https://media.discordapp.net/attachments/1061348902252597368/1115385822343069706/wescosmic_smart_online_classroom_for_university_students_future_e568a396-ea0d-4a3f-8b65-7ec3cfe82931.png?width=350&height=180' },
     { id: 5, username: 'Positive Social Impact', image: 'https://media.discordapp.net/attachments/1061348902252597368/1113936393211760660/wescosmic_people_doing_social_good_in_community_and_nature_caus_bcbd483f-47c4-4a60-a0ee-1c7ba704e1d4.png?width=350&height=180' },
   ];
 
