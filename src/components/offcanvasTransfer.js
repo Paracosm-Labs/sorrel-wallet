a98c8a84b5f342d29cc39e96704a49f87a0b6ef6
CosmicAnon <wescosmic@gmail.com>
2023-06-15 18:07:18 +0545

    Update offcanvasTransfer.js

--- src/components/offcanvasTransfer.js
+++ src/components/offcanvasTransfer.js
@@ -4,43 +4,79 @@ import Dialpad from './dialpad';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import LogoImg from '../img/logo2x.png';
+import OffcanvasPinpad from './offcanvasPinpad';
 
 const OffcanvasTransfer = ({ selectedSorrelAddress }) => {
   const [selectedDestination, setSelectedDestination] = useState('');
+  const [sendAmount, setSendAmount] = useState('');
+  const [pin, setPin] = useState('');
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+  const [closeTransferPane, setCloseTransferPane] = useState(null);
+  const [offcanvasTitle, setOffcanvasTitle] = useState('');
 
   const handleDestinationChange = (event) => {
     setSelectedDestination(event);
   };
 
-  const handleConfirmTransfer = (amount) => {
-    // Process transfer using the selected address
+  const handlePinConfirmation = (amount) => {
+    setOffcanvasTitle('Enter PIN to Confirm');
+    setSendAmount(amount);
+    setShowOffcanvas(true);
+    // alert("yea");
+  }
+
+  const resetPane  = () => {
+    setCloseTransferPane(false);
+  }
+
+  const handleConfirmTransfer = async () => {
+    // Process transfer using thet selected address
+    setShowOffcanvas(false)
+    setCloseTransferPane(true);
+
     if (selectedSorrelAddress) {
-        toast.success(`Sent ${amount} to ${selectedSorrelAddress}`, {
+        toast.success(`Sent ${sendAmount} to ${selectedSorrelAddress}`, {
           icon: ({theme, type}) =>  <img src={LogoImg} alt="Sorrel Logo" className="rounded-circle me-5" height="24"/>,
           theme: "dark",
         });
-        console.log(`Transferred ${amount}, to address: ${selectedSorrelAddress}`);
+        console.log(`Transferred ${sendAmount}, to address: ${selectedSorrelAddress}`);
     } else {
-        toast.success(`Sent ${amount} to ${selectedDestination}`, {
+        toast.success(`Sent ${sendAmount} to ${selectedDestination}`, {
           icon: ({theme, type}) =>  <img src={LogoImg} alt="Logo" className="rounded-circle me-5" height="24"/>,
           theme: "dark",
         });
-        console.log(`Transferred ${amount}, to address: ${selectedDestination}`);
+        console.log(`Transferred ${sendAmount}, to address: ${selectedDestination}`);
     }
   };
 
   return (
     <>
-      <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasTransfer" aria-labelledby="offcanvasTransferLabel">
+      <div className={`offcanvas offcanvas-end`} tabIndex="-1" id="offcanvasTransfer" aria-labelledby="offcanvasTransferLabel">
         <div className="offcanvas-header">
           <h5 className="offcanvas-title" id="offcanvasTransferLabel">Transfer Funds</h5>
           <button type="button" className="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div className="offcanvas-body">
+        {!closeTransferPane ? (<>
           <AddressBook onContactSelect={handleDestinationChange} sorrelAddress={selectedSorrelAddress}/>
-          <Dialpad onConfirm={handleConfirmTransfer} selectedDestination={selectedDestination} />
+          <Dialpad onConfirm={handlePinConfirmation} selectedDestination={selectedDestination} />
+          </>):(
+          <button onClick={resetPane} type="button" className={`mt-5 btn btn-lg w-100 btn-success`} data-bs-dismiss="offcanvas" aria-label="Close">
+            Finish
+          </button>
+          )}
         </div>
       </div>
+
+      <OffcanvasPinpad 
+        showOffcanvas={showOffcanvas} 
+        setShowOffcanvas={setShowOffcanvas} 
+        offcanvasTitle={offcanvasTitle} 
+        pin={pin} 
+        handleOffcanvasSubmit={handleConfirmTransfer} 
+      />
+
+
       <ToastContainer
         position="top-center"
         autoClose={3000}
