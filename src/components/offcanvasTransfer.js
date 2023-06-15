4ce3d779fc0c9f650ecbae5ee320d9151c8a7682
CosmicAnon <wescosmic@gmail.com>
2023-06-15 18:30:52 +0545

    transfer updates

--- src/components/offcanvasTransfer.js
+++ src/components/offcanvasTransfer.js
@@ -51,7 +51,7 @@ const OffcanvasTransfer = ({ selectedSorrelAddress }) => {
 
   return (
     <>
-      <div className={`offcanvas offcanvas-end`} tabIndex="-1" id="offcanvasTransfer" aria-labelledby="offcanvasTransferLabel">
+      <div className={`offcanvas transfer offcanvas-end`} tabIndex="-1" id="offcanvasTransfer" aria-labelledby="offcanvasTransferLabel">
         <div className="offcanvas-header">
           <h5 className="offcanvas-title" id="offcanvasTransferLabel">Transfer Funds</h5>
           <button type="button" className="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -61,10 +61,11 @@ const OffcanvasTransfer = ({ selectedSorrelAddress }) => {
           <AddressBook onContactSelect={handleDestinationChange} sorrelAddress={selectedSorrelAddress}/>
           <Dialpad onConfirm={handlePinConfirmation} selectedDestination={selectedDestination} />
           </>):(
+          <><div className="text-center m-5"><i className="fa fa-solid fa-circle-check fa-lg text-success"></i></div>
           <button onClick={resetPane} type="button" className={`mt-5 btn btn-lg w-100 btn-success`} data-bs-dismiss="offcanvas" aria-label="Close">
             Finish
           </button>
-          )}
+          </>)}
         </div>
       </div>
 
@@ -72,7 +73,7 @@ const OffcanvasTransfer = ({ selectedSorrelAddress }) => {
         showOffcanvas={showOffcanvas} 
         setShowOffcanvas={setShowOffcanvas} 
         offcanvasTitle={offcanvasTitle} 
-        pin={pin} 
+        pin={pin}
         handleOffcanvasSubmit={handleConfirmTransfer} 
       />
 
